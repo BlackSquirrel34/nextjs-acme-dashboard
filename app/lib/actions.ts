49388dfef09d3b0edaab42dcf49b502a665e6e64
BlackSquirrel <Maexxla3458@proton.me>
2025-03-03 20:39:33 +0100

    error handling and feedback done

---
+++ app/lib/actions.ts
@@ -0,0 +1,117 @@
+'use server';
+import { z } from 'zod';
+import postgres from 'postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
+  date: z.string(),
+});
+
+export type State = {
+    errors?: {
+      customerId?: string[];
+      amount?: string[];
+      status?: string[];
+    };
+    message?: string | null;
+};
+ 
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    // Validate form fields using Zod
+    const validatedFields = CreateInvoice.safeParse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+
+     // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+        return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+
+    // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
+     // It's good practice to store monetary values in cents 
+    // to increase accuracy and avoid ploating point errors
+    const amountInCents = amount * 100;
+    // a new date with format "YYYY-MM-DD" for the invoice's creation date
+    const date = new Date().toISOString().split('T')[0];
+
+    // insert into DB with SQL query
+    try {
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+    } catch (error) {
+    // We'll log the error to the console for now
+    console.error(error);
+    } 
+
+    // Since you're updating the data displayed in the invoices route, 
+    // you want to clear this cache and trigger a new request to the server. 
+    // You can do this with the revalidatePath
+    revalidatePath('/dashboard/invoices');
+    // Once the database has been updated, the /dashboard/invoices path 
+    // will be revalidated, and fresh data will be fetched from the server.
+    // we then redirect the user back to dashboard
+    redirect('/dashboard/invoices');
+  }
+
+// Updating an invoice
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   
+    try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+    } catch (error) {
+    // We'll log the error to the console for now
+    console.error(error);
+    }
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
+
+// Delete an Invoice
+export async function deleteInvoice(id: string) {
+    try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+} catch (error) {
+    // We'll log the error to the console for now
+    console.error(error);
+    }
+  }
+
+
